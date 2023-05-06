c946278865c3539c3da79838c244964edf3ea607
Yuya Minamide <dettu1003@gmail.com>
2023-05-05 22:02:13 -0700

    deleted unnecessary comments

--- frontend/src/utility/ImagetoBase64.js
+++ frontend/src/utility/ImagetoBase64.js
@@ -1,15 +1,3 @@
-// export async function ImagetoBase64(file) {
-// 	const reader = new FileReader();
-// 	reader.readAsDataURL(file);
-
-// 	const data = new Promise((resolve, reject) => {
-// 		reader.onload = () => resolve(reader.result);
-// 		reader.onerror = (err) => reject(err);
-// 	});
-
-// 	return data;
-// }
-
 export async function ImagetoBase64(file) {
 	if (!file || !(file instanceof Blob)) {
 		throw new Error("Invalid file object");
