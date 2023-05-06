9838a23a0d6f4a8afde86addc972788504085a8c
Yuya Minamide <dettu1003@gmail.com>
2023-05-05 21:58:31 -0700

    added logic to AddFood component

--- frontend/src/pages/addfood/index.jsx
+++ frontend/src/pages/addfood/index.jsx
@@ -1,5 +1,6 @@
 import { Header } from "@/components";
 import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 import { AiOutlineCloudUpload } from "react-icons/ai";
 import styled from "styled-components";
 import { ImagetoBase64 } from "@/utility/ImagetoBase64";
@@ -128,36 +129,63 @@ const AddFood = () => {
 		});
 	};
 
-	const handleSubmit = (e) => {
+	const handleSubmit = async (e) => {
 		e.preventDefault();
-		console.log(data);
+
+		const { restaurantname, name, category, price, image, description } = data;
+		if (restaurantname && name && category && price && image && description) {
+			const fetchData = await fetch(`${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/addfood`, {
+				method: "POST",
+				headers: {
+					"content-type": "application/json",
+				},
+				body: JSON.stringify(data),
+			});
+
+			const resData = await fetchData.json();
+			toast.success(resData.message);
+
+			setData(() => {
+				return {
+					restaurantname: "",
+					name: "",
+					category: "",
+					price: "",
+					image: "",
+					description: "",
+				};
+			});
+		} else {
+			toast.error("Please enter required fields");
+		}
 	};
 
 	return (
 		<>
 			<Header />
+			<Toaster position="top-center" />
 			<Title>Add food</Title>
 			<Container>
 				<FormContainer>
 					<form onSubmit={handleSubmit}>
 						<div>
 							<label htmlFor="restaurantname">Restaurant name</label>
-							<input type="text" name="restaurantname" onChange={handleOnChange} />
+							<input type="text" name="restaurantname" value={data.restaurantname} onChange={handleOnChange} />
 						</div>
 
 						<div>
 							<label htmlFor="name">Name</label>
-							<input type="text" name="name" onChange={handleOnChange} />
+							<input type="text" name="name" value={data.name} onChange={handleOnChange} />
 						</div>
 
 						<div>
 							<label htmlFor="category">Category</label>
-							<select id="category" name="category" onChange={handleOnChange}>
+							<select id="category" name="category" value={data.category} onChange={handleOnChange}>
 								<option value="select">Select</option>
 								<option value="japanese">Japanese</option>
 								<option value="chinese">Chinese</option>
-								<option value="indian">Indian chicken</option>
-								<option value="american">American noodle</option>
+								<option value="indian">Indian</option>
+								<option value="american">American</option>
 								<option value="mexican">Mexican</option>
 								<option value="other">Other</option>
 							</select>
@@ -165,14 +193,14 @@ const AddFood = () => {
 
 						<div>
 							<label htmlFor="price">Price</label>
-							<input type="text" name="price" onChange={handleOnChange} />
+							<input type="text" name="price" value={data.price} onChange={handleOnChange} />
 						</div>
 
 						<div>
 							<label htmlFor="image">
 								Image
 								<div>
-									<input type="file" id="image" name="image" onChange={uploadImage} />
+									<input type="file" accept="image/*" id="image" name="image" onChange={uploadImage} />
 									{data.image ? (
 										<img src={data.image} />
 									) : (
@@ -186,7 +214,7 @@ const AddFood = () => {
 
 						<div>
 							<label htmlFor="description">Description</label>
-							<textarea rows="2" name="description" onChange={handleOnChange}></textarea>
+							<textarea rows="2" name="description" value={data.description} onChange={handleOnChange}></textarea>
 						</div>
 
 						<button>Save</button>
