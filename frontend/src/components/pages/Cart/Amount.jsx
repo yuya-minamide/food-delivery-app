71e362112a2f4b83b22a3407c942423a1426e0df
Yuya Minamide <dettu1003@gmail.com>
2023-05-10 16:02:22 -0700

    fixed logic if the cart is empty, you cannot click Buy button

--- frontend/src/components/pages/Cart/Amount.jsx
+++ frontend/src/components/pages/Cart/Amount.jsx
@@ -27,7 +27,9 @@ export function Amount() {
 		<AmountContainer>
 			<p>Total quantity: {totalQuantity}</p>
 			<p>Total amount: ${totalAmount}</p>
-			<button onClick={handleBuy}>Buy</button>
+			<button disabled={totalQuantity === 0} onClick={handleBuy}>
+				Buy
+			</button>
 		</AmountContainer>
 	);
 }
